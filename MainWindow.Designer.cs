7bee2c4c1bdf874462f3ace249b812767056631f
Bryan <bryan@newlinetechnicalinnovations.com>
2014-11-10 14:54:54 -0700

    Build test framework. Create first two tests.

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -29,22 +29,142 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.ourText = new System.Windows.Forms.RichTextBox();
+            this.testNumber = new System.Windows.Forms.Label();
+            this.theirText = new System.Windows.Forms.RichTextBox();
+            this.runButton = new System.Windows.Forms.Button();
+            this.successButton = new System.Windows.Forms.Button();
+            this.failedButton = new System.Windows.Forms.Button();
+            this.testDescription = new System.Windows.Forms.TextBox();
+            this.testCountdown = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // ourText
+            // 
+            this.ourText.Location = new System.Drawing.Point(17, 198);
+            this.ourText.Name = "ourText";
+            this.ourText.Size = new System.Drawing.Size(486, 500);
+            this.ourText.TabIndex = 0;
+            this.ourText.Text = "";
+            // 
+            // testNumber
+            // 
+            this.testNumber.AutoSize = true;
+            this.testNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.testNumber.Location = new System.Drawing.Point(12, 9);
+            this.testNumber.Name = "testNumber";
+            this.testNumber.Size = new System.Drawing.Size(193, 29);
+            this.testNumber.TabIndex = 1;
+            this.testNumber.Text = "Test Number: 0";
+            // 
+            // theirText
+            // 
+            this.theirText.Location = new System.Drawing.Point(510, 198);
+            this.theirText.Name = "theirText";
+            this.theirText.Size = new System.Drawing.Size(486, 500);
+            this.theirText.TabIndex = 3;
+            this.theirText.Text = "";
+            // 
+            // runButton
+            // 
+            this.runButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.runButton.Location = new System.Drawing.Point(466, 704);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(84, 30);
+            this.runButton.TabIndex = 4;
+            this.runButton.Text = "Run Test";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // successButton
+            // 
+            this.successButton.Enabled = false;
+            this.successButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.successButton.Location = new System.Drawing.Point(423, 704);
+            this.successButton.Name = "successButton";
+            this.successButton.Size = new System.Drawing.Size(80, 30);
+            this.successButton.TabIndex = 5;
+            this.successButton.Text = "Success";
+            this.successButton.UseVisualStyleBackColor = true;
+            this.successButton.Visible = false;
+            this.successButton.Click += new System.EventHandler(this.successButton_Click);
+            // 
+            // failedButton
+            // 
+            this.failedButton.Enabled = false;
+            this.failedButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.failedButton.Location = new System.Drawing.Point(510, 704);
+            this.failedButton.Name = "failedButton";
+            this.failedButton.Size = new System.Drawing.Size(80, 30);
+            this.failedButton.TabIndex = 6;
+            this.failedButton.Text = "Failed";
+            this.failedButton.UseVisualStyleBackColor = true;
+            this.failedButton.Visible = false;
+            this.failedButton.Click += new System.EventHandler(this.failedButton_Click);
+            // 
+            // testDescription
+            // 
+            this.testDescription.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.testDescription.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.testDescription.Enabled = false;
+            this.testDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.testDescription.Location = new System.Drawing.Point(17, 41);
+            this.testDescription.Multiline = true;
+            this.testDescription.Name = "testDescription";
+            this.testDescription.ReadOnly = true;
+            this.testDescription.Size = new System.Drawing.Size(979, 128);
+            this.testDescription.TabIndex = 7;
+            this.testDescription.Text = "Detailed Test description goes here.";
+            // 
+            // testCountdown
+            // 
+            this.testCountdown.AutoSize = true;
+            this.testCountdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.testCountdown.Location = new System.Drawing.Point(357, 172);
+            this.testCountdown.Name = "testCountdown";
+            this.testCountdown.Size = new System.Drawing.Size(233, 20);
+            this.testCountdown.TabIndex = 8;
+            this.testCountdown.Text = "Test will begin in: 5 seconds";
+            this.testCountdown.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(1008, 740);
+            this.Controls.Add(this.testCountdown);
+            this.Controls.Add(this.testDescription);
+            this.Controls.Add(this.failedButton);
+            this.Controls.Add(this.successButton);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.theirText);
+            this.Controls.Add(this.testNumber);
+            this.Controls.Add(this.ourText);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "MainWindow";
-            this.Text = "Form1";
+            this.Text = "EHR Monitor Test";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.RichTextBox ourText;
+        private System.Windows.Forms.Label testNumber;
+        private System.Windows.Forms.RichTextBox theirText;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.Button successButton;
+        private System.Windows.Forms.Button failedButton;
+        private System.Windows.Forms.TextBox testDescription;
+        private System.Windows.Forms.Label testCountdown;
         private System.Windows.Forms.Timer timer1;
     }
 }
--- MainWindow.cs
+++ MainWindow.cs
@@ -13,13 +13,278 @@ namespace EHR_Monitor_Test
 {
     public partial class MainWindow : Form
     {
-        private int ticks = 5;  // How many seconds do we want to allow a user to find the EHR Window before attempting to poll it
+        private const int ticks = 5;  // How many seconds do we want to allow a user to find the EHR Window before attempting to poll it
+        private int currentTick = 0;
+
+        private const int numberOfTests = 4;
+        private bool[] results = new bool[numberOfTests  + 1];
+
+        private int currentTestNumber = 0;  // What test are we currently on
+        private delegate void TestDelegate();
+        private TestDelegate currentTest = null;
+        private bool warned = false;  // Have we warned the user to move their cursor focus to the EHR window already for this test?
 
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+            this.timer1.Stop();
 
-            timer1.Stop();
+            // Don't allow for maximizing the window as the layout is static...
+            this.MaximizeBox = false;
+
+            // Set any atributes on our own items that we may need
+            this.ourText.ReadOnly = true;
+            this.theirText.ReadOnly = true;
+
+            // Just for easier reading of results...
+            results[0] = true;
+
+            // Go ahead and setup the first test in the view.
+            this.SetupNextTest();
+
+            MessageBox.Show("On all tests, after pushing the 'Run Test' button, you will have 5 seconds to bring your EHR Application to the foreground and click into the text box (make sure it has focus). \n\nFor these tests to work, the cursor must be in the main text box like you were actively working or typing in it.");
+        }
+
+        private void SetupTestOne()
+        {
+            this.testNumber.Text = "Test Number: 1";
+            this.testDescription.Text = "First, we are going to test if we can successfully READ text from the text area inside the EHR Application. \r\nTo do this, YOU need to manually insert text into the EHR Application. The text on the left side below has been copied to your clipboard. \r\nPlease open your EHR Application and PASTE the text into the normal working area. \r\n\r\nThe test will attempt to read text from the EHR Application and place it on the right side. \r\nSuccess is when the right side contains the text from the left.";
+
+            this.ourText.Text = "Test One \r\nSimple Text Test \r\n\r\nThis is a test of reading text from the EHR Application.";
+            
+            Clipboard.SetText(this.ourText.Text, TextDataFormat.Text);
+        }
+
+        private void RunTestOne()
+        {
+            if (!this.warned)
+            {
+                MessageBox.Show("Please make sure you have pasted the text from the left box into your EHR Application. \nOnce you push 'Ok', you will have 5 seconds to click into your EHR's main Text Box.");
+                this.ShowTestCountdown();
+            }
+            else
+            {
+                // Read the text from the EHR Application
+                string readText = EHRText.GetTextFromActiveWindowElement();
+                // Set the right side text to what we read
+                this.theirText.Text = readText;
+
+                if (this.theirText.Text.Contains(this.ourText.Text))
+                {
+                    this.PrintPossibleSuccess();
+                }
+                else
+                {
+                    this.PrintPossibleFailure();
+                }
+            }
+        }
+
+        private void SetupTestTwo()
+        {
+            this.testNumber.Text = "Test Number: 2";
+            this.testDescription.Text = "Next, we are going to test if we can successfully WRITE text to the text area inside the EHR Application. \r\nTo do this, WE will need to insert text into the EHR Application. \r\n\r\nThis test will attempt to write the text from the left side below into your EHR's Text Box. It will then read back your EHR's text and paste the read in value to the right side below. \r\nSuccess is when the right side matches the left side, AND the Text Box in the EHR Application contains the same text.";
+
+            this.ourText.Text = "Test Two; Test of writing text. The left and right text boxes should match once the test has run.";
+        }
+
+        private void RunTestTwo()
+        {
+            if (!this.warned)
+            {
+                MessageBox.Show("Once you push 'Ok', you will have 5 seconds to click into your EHR's main Text Box.");
+                this.ShowTestCountdown();
+            }
+            else
+            {
+                // Write our text to the EHR
+                EHRText.SetTextOfActiveWindowElement(ourText.Text);
+
+                // Read the text back from the EHR Application
+                string readText = EHRText.GetTextFromActiveWindowElement();
+                // Set the right side text to what we read
+                this.theirText.Text = readText;
+
+                if (this.theirText.Text.Equals(this.ourText.Text))
+                {
+                    this.PrintPossibleSuccess();
+                }
+                else
+                {
+                    this.PrintPossibleFailure();
+                }
+            }
+        }
+
+        private void PrintPossibleSuccess()
+        {
+            MessageBox.Show("It looks like the test was a success! \nIf you agree, please push the 'Success' button and continue.");
+        }
+
+        private void PrintPossibleFailure()
+        {
+            MessageBox.Show("It looks like the test was a failure... \nIf you agree, please push the 'Faied' button and report the issue.");
+        }
+
+        private void ShowTestCountdown()
+        {
+            this.testCountdown.Text = "Test will begin in: " + ticks + " seconds";
+            this.testCountdown.Enabled = true;
+            this.testCountdown.Visible = true;
+
+            this.warned = true;
+
+            this.timer1.Start();
+        }
+
+        private void EnableRunButton()
+        {
+            this.runButton.Visible = true;
+            this.runButton.Enabled = true;
+
+            this.successButton.Enabled = false;
+            this.successButton.Visible = false;
+            this.failedButton.Enabled = false;
+            this.failedButton.Visible = false;
+        }
+
+        private void EnableResultButtons()
+        {
+            this.runButton.Visible = false;
+            this.runButton.Enabled = false;
+
+            this.successButton.Enabled = true;
+            this.successButton.Visible = true;
+            this.failedButton.Enabled = true;
+            this.failedButton.Visible = true;
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            switch (this.currentTestNumber)
+            {
+                case 1:
+                    RunTestOne();
+                    break;
+                case 2:
+                    RunTestTwo();
+                    break;
+                case 3:
+                    //RunTestThree();
+                    break;
+                case 4:
+                    //RunTestFour();
+                    break;
+                default:
+                    EnableRunButton();
+                    break;
+            }
+        }
+
+        private void ContinueCurrentTest()
+        {
+            this.currentTest();
+        }
+
+        private void SetupNextTest()
+        {
+            this.warned = false;
+            this.EnableRunButton();
+
+            this.ourText.Text = "";
+            this.theirText.Text = "";
+
+            switch (this.currentTestNumber)
+            {
+                case 0:
+                    this.currentTestNumber = 1;
+                    this.currentTest = RunTestOne;
+
+                    SetupTestOne();
+                    break;
+                case 1:
+                    this.currentTestNumber = 2;
+                    this.currentTest = RunTestTwo;
+
+                    SetupTestTwo();
+                    break;
+                case 2:
+                    this.currentTestNumber = 3;
+                    //this.currentTest = RunTestThree;
+
+                    //SetupTestThree();
+                    break;
+                case 3:
+                    this.currentTestNumber = 4;
+                    //this.currentTest = RunTestFour;
+
+                    //SetupTestFour();
+                    break;
+                case numberOfTests:
+                    ShowResults();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ShowResults()
+        {
+            this.runButton.Enabled = false;
+            this.runButton.Visible = false;
+            this.successButton.Enabled = false;
+            this.successButton.Visible = false;
+            this.failedButton.Enabled = false;
+            this.failedButton.Visible = false;
+
+            this.testNumber.Text = "Test Number: Done";
+            this.testDescription.Text = "Thank you for running all of our tests. \r\nPlease report back the below results.";
+
+            this.ourText.Text = "Results: \r\n\r\n" + this.results.ToString();
+
+            if (Array.TrueForAll(this.results, IsSuccess))
+            {
+                this.theirText.Text = "Good!";
+            }
+            else
+            {
+                this.theirText.Text = "Bad...";
+            }
+        }
+
+        private bool IsSuccess(bool value)
+        {
+            return value;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.currentTick >= ticks)
+            {
+                this.timer1.Stop();
+                this.currentTick = 0;
+                this.testCountdown.Enabled = false;
+                this.testCountdown.Visible = false;
+
+                this.ContinueCurrentTest();
+            }
+            else
+            {
+                this.currentTick++;
+                this.testCountdown.Text = "Test will begin in: " + (ticks - this.currentTick) + " seconds";
+            }
+        }
+
+        private void successButton_Click(object sender, EventArgs e)
+        {
+            this.results[this.currentTestNumber] = true;
+            this.SetupNextTest();
+        }
+
+        private void failedButton_Click(object sender, EventArgs e)
+        {
+            this.results[this.currentTestNumber] = false;
+            this.SetupNextTest();
         }
     }
 }
