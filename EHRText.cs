37f5fc04a13421af45abc90f2584b3f393da72cf
Bryan <bryan@newlinetechnicalinnovations.com>
2014-11-11 11:29:11 -0700

    Add initial code to attempting reading of RTF from external RichTextBoxes.

--- EHRText.cs
+++ EHRText.cs
@@ -7,11 +7,19 @@ using System.Text;
 // Additional Imports used by the EHR Monitor Test
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace EHR_Monitor_Test
 {
     public class EHRText
     {
+        private const uint WM_SETTEXT = 0x000C;
+        private const uint WM_GETTEXT = 0x000D;
+        private const uint WM_GETTEXTLENGTH = 0x000E;
+        private const uint WM_USER = 0x0400;
+        private const uint EM_STREAMOUT = WM_USER + 74;
+        private const uint SF_RTF = 2;
+
         [DllImport("user32.dll", EntryPoint="GetForegroundWindow")]
         private static extern IntPtr GetForegroundWindow();
 
@@ -21,11 +29,33 @@ namespace EHR_Monitor_Test
         [DllImport("user32.dll", EntryPoint="GetGUIThreadInfo", SetLastError=true)]
         private static extern bool GetGUIThreadInfo(IntPtr hThreadID, ref GUITHREADINFO lpgui);
 
-        [DllImport("user32.dll", EntryPoint="SendMessage", CharSet = CharSet.Auto)]
+        [DllImport("user32.dll", EntryPoint="SendMessage", CharSet=CharSet.Auto)]
         private static extern IntPtr SendMessage(IntPtr hWnd, uint msg, IntPtr wParam, [Out] StringBuilder lParam);
 
-        [DllImport("user32.dll", EntryPoint="SendMessage", CharSet = CharSet.Auto)]
-        private static extern IntPtr SendMessage(IntPtr hWnd, uint msg, int wParam, [Out] string lParam);
+        [DllImport("user32.dll", EntryPoint="SendMessage", CharSet=CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint msg, uint wParam, [Out] string lParam);
+
+        [DllImport("user32.dll", EntryPoint = "SendMessage", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint msg, uint wParam, ref EDITSTREAM lParam);
+
+        private delegate int EditStreamCallback(MemoryStream dwCookie, IntPtr pbBuff, int cb, out int pcb);
+
+        private static int EditStreamProc(MemoryStream dwCookie, IntPtr pbBuff, int cb, out int pcb)
+        {
+            pcb = cb;
+            byte[] buffer = new byte[cb];
+            Marshal.Copy(pbBuff, buffer, 0, cb);
+            dwCookie.Write(buffer, 0, cb);
+            return 0;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private class EDITSTREAM
+        {
+            public MemoryStream dwCookie;
+            public uint dwError;
+            public EditStreamCallback pfnCallback;
+        }
 
         private struct RECT
         {
@@ -50,17 +80,12 @@ namespace EHR_Monitor_Test
 
         private static void SetText(IntPtr handle, string text)
         {
-            const uint WM_SETTEXT = 0x000C;
-
             SendMessage(handle, WM_SETTEXT, 0, text);
         }
 
         // Gets text text from a control by it's handle.
         private static string GetText(IntPtr handle)
         {
-            const uint WM_GETTEXTLENGTH = 0x000E;
-            const uint WM_GETTEXT = 0x000D;
-
             // Gets the text length.
             var length = (int)SendMessage(handle, WM_GETTEXTLENGTH, IntPtr.Zero, null);
 
@@ -115,6 +140,44 @@ namespace EHR_Monitor_Test
             catch (Exception e)
             {
                 Console.Write(e.Message);
+            }
+        }
+
+        public static string GetRTFFromActiveWindowElement()
+        {
+            try
+            {
+                IntPtr windowHWnd = GetForegroundWindow();
+                IntPtr lpdwProcessId;
+                IntPtr threadId = GetWindowThreadProcessId(windowHWnd, out lpdwProcessId);
+
+                GUITHREADINFO lpgui = new GUITHREADINFO();
+                lpgui.cbSize = Marshal.SizeOf(lpgui);
+
+                GetGUIThreadInfo(threadId, ref lpgui);
+
+                string result = String.Empty;
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    EDITSTREAM editStream = new EDITSTREAM();
+                    editStream.pfnCallback = new EditStreamCallback(EditStreamProc);
+                    editStream.dwCookie = stream;
+
+                    SendMessage(lpgui.hwndFocus, EM_STREAMOUT, SF_RTF, ref editStream);
+
+                    stream.Seek(0, SeekOrigin.Begin);
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        result = reader.ReadToEnd();
+                    }
+                }
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+                return null;
             }
         }
     }
